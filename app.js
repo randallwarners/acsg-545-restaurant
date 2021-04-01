77d8e2cd0875224fdf8e4ec0b7a6b6409d975f65
Randall Warners <warners.r01@mymail.sxu.edu>
2021-03-31 20:09:10 -0500

    enforce foreign key constraints

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 
 // database
-app.set('db', db)
+app.set('db', db.exec('PRAGMA foreign_keys = 1;'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

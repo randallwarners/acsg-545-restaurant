92ba3a1f69316bf512ddbe1cabbebb348e540852
Jdavidrozo <78281586+Jdavidrozo@users.noreply.github.com>
2021-04-07 18:07:58 -0500

    Update menu_item_mapper.js

--- models/menu_item_mapper.js
+++ models/menu_item_mapper.js
@@ -4,21 +4,53 @@ class MenuItemMapper {
       {
         id: 1,
         name: 'taco',
-        description: 'delicious taco',
+        description: 'Your choice of meat served in a soft or hard shell tortilla with guac, salsa, sour cream, cheese and topped with hand-cut lettuce',
         image: 'images/taco.png'
       },
       {
         id: 2,
-        name: 'burrito',
-        description: 'delicious burrito',
+        name: 'Burrito',
+        description: 'Your choice of chicken, beef or fish wrapped in a warm flour, tortilla with rice, lettuce, fajita veggies and topped with guac, salsa, queso, sour cream or cheese.',
         image: 'images/burrito.png'
       },
       {
         id: 3,
-        name: 'bottled water',
-        description: 'fresh water',
-        image: 'images/water.png'
+        name: 'Burrito Bowls',
+        description:'Your choice of chicken, beef or fish served in a delicious bowl with rice, lettuce, fajita veggies and topped with guac, salsa, queso, sour cream or cheese.',
+        image: 'images/bowl.png'
+      },
+      {
+        id: 4,
+        name: 'Side of Fresh Guacamole',
+        description: 'Delicious Guacamole',
+        image: 'images/side.png'
+      },
+      {
+        id: 5,
+        name: 'Chips',
+        description: 'Delicious Side Chips',
+        image: 'images/side.png'
+      },
+      {
+        id: 6, 
+        name: 'Chips & Salsa',
+        description: 'Delicious Chips and Salsa',
+        image: 'images/side.png'
+      },
+      {
+        id: 7,
+        name: 'Mexican Coca-Cola',
+        description: 'Tasty soda to go with your meal',
+        image: 'images/drink.png'
+      },
+      {
+        id: 8, 
+        name: 'Bottled Water',
+        description: 'Refreshing water',
+        image: 'images/drink.png'
       }
+        
+        
     ]
   }
 }

80fd8f11e9b9b56688e92852ec9aeed3c320d498
Randall Warners <warners.r01@mymail.sxu.edu>
2021-04-09 21:57:31 -0500

    style cart

--- models/order_mapper.js
+++ models/order_mapper.js
@@ -3,29 +3,29 @@ class OrderMapper {
     return [
       {
         id: 1,
-        name: 'taco',
-        image: 'images/taco.png',
+        name: 'Taco',
+        image: '/images/taco.png',
         price: 6,
         quantity: 1
       },
       {
         id: 3,
-        name: 'Burrito Bowls',
-        image: 'images/bowl.png',
+        name: 'Burrito Bowl',
+        image: '/images/bowl.png',
         price: 8,
         quantity: 1
       },
       {
         id: 4,
         name: 'Side of Fresh Guacamole',
-        image: 'images/side.png',
+        image: '/images/side.png',
         price: 2,
         quantity: 1
       },
       {
         id: 8,
         name: 'Bottled Water',
-        image: 'images/drink.png',
+        image: '/images/drink.png',
         price: 1.5,
         quantity: 1
       }
